a0dec80f26939aef59f16a9a543f8eaef180a585
AndyZ <zhangy01@softin-tech.com>
2022-11-24 09:42:57 +0800

    登出

--- src/components/Layout/header.jsx
+++ src/components/Layout/header.jsx
@@ -1,12 +1,36 @@
 import clsx from "clsx";
-import LogoDevRounded from "@mui/icons-material/LogoDevRounded";
 import Link from "next/link";
-import Dropdown from "../dropdown/dropdown";
-import { Breadcrumbs } from "@mui/material";
+import {
+  Avatar,
+  Breadcrumbs,
+  IconButton,
+  Menu,
+  MenuItem,
+  MenuList,
+  Tooltip,
+} from "@mui/material";
 import SettingsRoundedIcon from "@mui/icons-material/SettingsRounded";
+import { signOut, useSession } from "next-auth/react";
 import styles from "./header.module.scss";
+import { useState } from "react";
 
 const Header = ({ fixedNav }) => {
+  const { data, status } = useSession();
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+
+  const openUserMenu = (event) => {
+    setAnchorEl(event?.currentTarget);
+  };
+
+  const closeUserMenu = () => {
+    setAnchorEl(null);
+  };
+
+  const logoutUser = () => {
+    signOut();
+  };
+
   return (
     <header
       className={clsx(
@@ -34,7 +58,17 @@ const Header = ({ fixedNav }) => {
         <div>title</div>
       </div>
       <div>
-        <SettingsRoundedIcon className={clsx("text-[24px]")} />
+        {/* <SettingsRoundedIcon className={clsx("text-[24px]")} /> */}
+        <Tooltip title={data?.user?.name}>
+          <IconButton onClick={openUserMenu} sx={{ p: 0 }}>
+            <Avatar src={data?.user?.image}></Avatar>
+          </IconButton>
+        </Tooltip>
+        <Menu anchorEl={anchorEl} open={open} onClose={closeUserMenu}>
+          <MenuItem>
+            <div className="w-[50px] text-center text-[16px]" onClick={logoutUser}>退出</div>
+          </MenuItem>
+        </Menu>
       </div>
     </header>
   );
